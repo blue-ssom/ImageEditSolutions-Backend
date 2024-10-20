34a83e47702c9db7d0dab2151a2d60d0bf3add00
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-10-20 14:08:59 +0900

    feat: ProjectRepository 인터페이스 추가 및 업로드 아이디 존재 여부 확인 메서드 구현

---
+++ src/main/java/ImageEditSolutions/api_server/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package ImageEditSolutions.api_server.repository;
+
+import ImageEditSolutions.api_server.entity.Project;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    boolean existsByUploadId(String uploadId);
+
+}
