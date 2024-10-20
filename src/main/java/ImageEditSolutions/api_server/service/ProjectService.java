0c9aea0eaed85f774c7cb4fc63ece2a2a8267d06
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-10-20 15:51:06 +0900

    feat: 프로젝트 다운로드 서비스 및 구현 분리

--- src/main/java/ImageEditSolutions/api_server/service/ProjectService.java
+++ src/main/java/ImageEditSolutions/api_server/service/ProjectService.java
@@ -1,7 +1,9 @@
 package ImageEditSolutions.api_server.service;
 
 import ImageEditSolutions.api_server.dto.request.ProjectReqDto;
+import ImageEditSolutions.api_server.dto.response.ProjectResDto;
 
 public interface ProjectService {
     void saveProject(ProjectReqDto projectReqDto);
+    ProjectResDto downloadProject(String uploadId);
 }
