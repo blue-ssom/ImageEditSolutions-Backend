7ac5487b04a45a2b5fea91842aa6d377facc4f37
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-10-20 14:04:40 +0900

    feat: 프로젝트 엔티티 필드 추가

---
+++ src/main/java/ImageEditSolutions/api_server/entity/Project.java
@@ -0,0 +1,49 @@
+package ImageEditSolutions.api_server.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "upload_id")
+    private String uploadId;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Project(String uploadId, String imageUrl) {
+        this.uploadId = uploadId;
+        this.imageUrl = imageUrl;
+    }
+
+}
