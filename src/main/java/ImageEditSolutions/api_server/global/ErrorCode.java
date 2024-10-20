53d42e1acc28eb2b989cd34647a0e016e67c542d
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-10-20 15:59:21 +0900

    fix: 에러 코드 및 상태 코드 수정

--- src/main/java/ImageEditSolutions/api_server/global/ErrorCode.java
+++ src/main/java/ImageEditSolutions/api_server/global/ErrorCode.java
@@ -8,12 +8,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    EMPTY_UPLOAD_ID(HttpStatus.BAD_REQUEST, "ACCOUNT-001", "업로드 아이디를 입력해주세요."),
-    UPLOAD_ID_ALREADY_EXISTS(HttpStatus.CONFLICT, "ACCOUNT-002", "이미 존재하는 아이디입니다."),
+    EMPTY_UPLOAD_ID(HttpStatus.BAD_REQUEST, "UPLOAD-001", "업로드 아이디를 입력해주세요."),
+    UPLOAD_ID_ALREADY_EXISTS(HttpStatus.CONFLICT, "UPLOAD-002", "이미 존재하는 아이디입니다."),
+    UPLOAD_ID_NOT_FOUND(HttpStatus.NOT_FOUND,"UPLOAD-003", "해당 업로드 아이디를 찾을 수 없습니다."),
 
     EMPTY_FILE(HttpStatus.BAD_REQUEST, "FILE-001","파일이 첨부되지 않았습니다."),
     INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST,"FILE-002", "잘못된 파일 형식입니다. 허용되는 파일 형식: jpg, jpeg, png, gif."),
-    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "FILE-002", "파일 업로드 중 오류가 발생했습니다.");
+    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "FILE-003", "파일 업로드 중 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
