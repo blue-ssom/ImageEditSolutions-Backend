d436d2ae7ffc335103d4fb0d78476acaa415049d
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-10-21 15:38:02 +0900

    feat: API 메서드 수정 및 Swagger 문서화 어노테이션 추가

--- src/main/java/ImageEditSolutions/api_server/controller/ProjectController.java
+++ src/main/java/ImageEditSolutions/api_server/controller/ProjectController.java
@@ -1,29 +1,45 @@
 package ImageEditSolutions.api_server.controller;
 
-import ImageEditSolutions.api_server.dto.request.ProjectReqDto;
 import ImageEditSolutions.api_server.dto.response.ProjectResDto;
 import ImageEditSolutions.api_server.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Response Estimate", description = "Response Estimate API")
 public class ProjectController {
     @Autowired
     ProjectService projectService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> saveProject(ProjectReqDto projectReqDto){
-        projectService.saveProject(projectReqDto);
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "프로젝트 저장하기")
+    @Parameter(name = "uploadId", description = "프로젝트 업로드에 사용되는 고유 아이디")
+    @ApiResponse(responseCode = "200", description = "프로젝트 저장하기 성공")
+    public ResponseEntity<String> saveProject( @RequestParam("uploadId") String uploadId,
+            @Parameter(description = "프로젝트에 업로드할 이미지 파일. 허용되는 파일 형식: jpg, jpeg, png, gif")
+            @RequestParam("multipartFile") MultipartFile multipartFile){
+        projectService.saveProject(uploadId, multipartFile);
         return ResponseEntity.ok("프로젝트가 성공적으로 업로드되었습니다.");
     }
 
     @GetMapping("/download")
+    @Operation(summary = "프로젝트 가져오기")
+    @Parameter(name = "uploadId", description = "프로젝트 업로드에 사용되는 고유 아이디")
+    @ApiResponse(responseCode = "200", description = "프로젝트 가져오기 성공", content = @Content(schema = @Schema(implementation = ProjectResDto.class)))
     public ResponseEntity<ProjectResDto> downloadProject(@RequestParam String uploadId) {
         ProjectResDto projectResDto = projectService.downloadProject(uploadId);
         return ResponseEntity.ok(projectResDto);
