64ea45304eb7244ed418ae0809b7dc53ec4b18e1
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-10-21 15:40:20 +0900

    refactor: saveProject 메서드 파라미터 수정 및 구현 변경

--- src/main/java/ImageEditSolutions/api_server/service/ProjectService.java
+++ src/main/java/ImageEditSolutions/api_server/service/ProjectService.java
@@ -1,9 +1,9 @@
 package ImageEditSolutions.api_server.service;
 
-import ImageEditSolutions.api_server.dto.request.ProjectReqDto;
 import ImageEditSolutions.api_server.dto.response.ProjectResDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ProjectService {
-    void saveProject(ProjectReqDto projectReqDto);
+    void saveProject(String uploadId, MultipartFile multipartFile);
     ProjectResDto downloadProject(String uploadId);
 }
