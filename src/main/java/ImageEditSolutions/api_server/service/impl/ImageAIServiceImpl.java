5c3a9b089460292e5b9a01087b55454f51ba93ef
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-11-01 19:24:19 +0900

    feat: ImageAIServiceImpl 클래스 구현

---
+++ src/main/java/ImageEditSolutions/api_server/service/impl/ImageAIServiceImpl.java
@@ -0,0 +1,38 @@
+package ImageEditSolutions.api_server.service.impl;
+
+import ImageEditSolutions.api_server.service.ImageAIService;
+import java.util.ArrayList;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ImageAIServiceImpl implements ImageAIService {
+
+    // 이미지 생성 요청 URL
+    private static final String IMAGE_AI_URL = "https://image.pollinations.ai/prompt/";
+
+    @Autowired
+    RestTemplate restTemplate; // URL 주소로 요청을 보낼 수 있는 라이브러리
+
+    @Override
+    public List<byte[]> createImageAI(String text) {
+        List<byte[]> responses = new ArrayList<>();
+
+        // 총 4개의 이미지를 생성하기 위해 반복문을 이용함
+        for (int i = 0; i < 4; i++) {
+            byte[] response = restTemplate.getForEntity(
+                    IMAGE_AI_URL + text + i,
+                    byte[].class
+            ).getBody();
+            responses.add(response);
+        }
+
+        return responses;
+    }
+
+}
