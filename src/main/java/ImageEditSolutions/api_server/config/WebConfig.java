578e4c11d209219373729242af5280506d427a4f
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-11-04 20:28:16 +0900

    feat: CORS 설정 개선

--- src/main/java/ImageEditSolutions/api_server/config/WebConfig.java
+++ src/main/java/ImageEditSolutions/api_server/config/WebConfig.java
@@ -2,15 +2,17 @@ package ImageEditSolutions.api_server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin", "Content-Type", "Accept");
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("GET", "POST") // 허용할 HTTP 메서드
+                .allowedHeaders("Origin", "Content-Type", "Accept"); // 허용할 헤더
     }
 }
